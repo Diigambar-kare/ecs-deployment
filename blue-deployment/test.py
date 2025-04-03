20668d6b8f255f4c7ad950c6caff5ad310c3f51a
Diigambar-kare <karedigambar@outlook.com>
2025-04-03 08:06:08 +0000

    FF

---
+++ blue-deployment/test.py
@@ -0,0 +1,23 @@
+import time
+import datetime
+import logging
+import random
+from prometheus_client import start_http_server, Counter
+from fastapi import FastApi
+
+app=FastApi()
+
+# Setup logging configuration to log to a file
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', filename='/var/log/my_python_app.log')
+
+# Prometheus counters
+hai_counter = Counter('hai_prints_total', 'Total number of times "Hai" was printed')
+error_counter = Counter('api_errors_total', 'Total number of API errors')
+
+
+@app.get("/")
+def health_check():
+    return{"status":"ok"}
+
+
+
