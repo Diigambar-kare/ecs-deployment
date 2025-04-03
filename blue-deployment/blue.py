f9798fef33f7b81de30061ea759a4e52a8ff5faf
Diigambar-kare <karedigambar@outlook.com>
2025-04-03 15:51:08 +0530

    Update blue.py
    mn

--- blue-deployment/blue.py
+++ blue-deployment/blue.py
@@ -1,23 +1,7 @@
-import time
-import datetime
-import logging
-import random
-from prometheus_client import start_http_server, Counter
 from fastapi import FastAPI
 
-app=FastAPI()
-
-# Setup logging configuration to log to a file
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', filename='/var/log/my_python_app.log')
-
-# Prometheus counters
-hai_counter = Counter('hai_prints_total', 'Total number of times "Hai" was printed')
-error_counter = Counter('api_errors_total', 'Total number of API errors')
-
+app = FastAPI()
 
 @app.get("/")
-def health_check():
-    return{"status":"ok"}
-
-
-
+def read_root():
+    return {"status":"ok"}
